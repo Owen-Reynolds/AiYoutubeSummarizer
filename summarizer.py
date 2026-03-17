92fd69a083ae3269d9e1a57f2fdd359c240eccaa
Owen-Reynolds <oreynold@ashland.edu>
2026-03-17 15:59:06 -0400

    Simple API Call

---
+++ summarizer.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def testCall():
+        response = client.chat.completions.create(
+                model = "gpt-4o-mini",
+                messages = [{"role": "user", "content": "Say hello!"}]
+        )
+
+        print(response.choices[0].message.content)
+
+
+testCall()
