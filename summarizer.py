0aaac1b1168fb53133c587bf6e585569eead319c
Owen-Reynolds <oreynold@ashland.edu>
2026-03-17 16:43:36 -0400

    Get Video ID and Transcript Functions

--- summarizer.py
+++ summarizer.py
@@ -1,10 +1,13 @@
 from openai import OpenAI
+from youtube_transcript_api import YouTubeTranscriptApi
 from dotenv import load_dotenv
-import os
+import os, re
 
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+
+#Test api
 def testCall():
         response = client.chat.completions.create(
                 model = "gpt-4o-mini",
@@ -13,5 +16,24 @@ def testCall():
 
         print(response.choices[0].message.content)
 
+#Get Youtube video IDs
+def getVideoID(url : str) -> str:
+        patterns = [
+                r"youtu\.be/([^?&]+)",
+                r"v=([^?&]+)"
+        ]
 
-testCall()
+        for p in patterns:
+                match = re.search(p, url)
+                if match: return match.group(1)
+        raise ValueError("Invalid Youtube URL")
+
+def getTranscript(url : str) -> str:
+        videoID = getVideoID(url)
+        transcript = YouTubeTranscriptApi.get_transcript(videoID)
+
+        texts = []
+        for t in transcript:
+                texts.append(t["text"])
+
+        return " ".join(texts)
